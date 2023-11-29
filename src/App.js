ebb4ed659598fdf608ee2af971efcf9d782030d8
Alex Hidalgo <alexjhidalgo@icloud.com>
2023-11-29 04:27:44 -0500

    Icons and Labels both display component

--- src/App.js
+++ src/App.js
@@ -14,49 +14,45 @@ library.add(faSignHanging, faHouse, faHouseUser, faCommentsDollar);
 
 
 function App() {
-  const [activeComponent, setActiveComponent] = useState('listings');
+  const [activeComponent, setActiveComponent] = useState('');
 
   const handleComponentChange = (componentName) => {
     setActiveComponent(componentName);
   };
   return (
     <div className="grid-container">
-      <div className={`icon listings ${activeComponent === 'listings' ? 'active' : ''}`}>
+      <div className={`icon listings ${activeComponent === 'listings' ? 'active' : ''}`} onClick={() => handleComponentChange('listings')}>
         {activeComponent === 'listings' && <Listings />}
         <FontAwesomeIcon
           icon={faSignHanging}
           style={{ color: "#00c28d", width: "15%", height: "15%", padding: "20px" }}
-          onClick={() => handleComponentChange('listings')}
         />
         <label>My Listings</label>
       </div>
 
-      <div className={`icon potentials ${activeComponent === 'potentials' ? 'active' : ''}`}>
+      <div className={`icon potentials ${activeComponent === 'potentials' ? 'active' : ''}`} onClick={() => handleComponentChange('potentials')}>
         {activeComponent === 'potentials' && <Potentials />}
         <FontAwesomeIcon
           icon={faHouse}
           style={{ color: "#5284dc", width: "15%", height: "15%", padding: "20px" }}
-          onClick={() => handleComponentChange('potentials')}
         />
         <label>Potential Acquisitions</label>
       </div>
 
-      <div className={`icon occupieds ${activeComponent === 'occupieds' ? 'active' : ''}`}>
+      <div className={`icon occupieds ${activeComponent === 'occupieds' ? 'active' : ''}`} onClick={() => handleComponentChange('occupieds')}>
         {activeComponent === 'occupieds' && <Occupieds />}
         <FontAwesomeIcon
           icon={faHouseUser}
           style={{ color: "#da9429", width: "15%", height: "15%", padding: "20px" }}
-          onClick={() => handleComponentChange('occupieds')}
         />
         <label>Property Management</label>
       </div>
 
-      <div className={`icon cashflows ${activeComponent === 'cashflows' ? 'active' : ''}`}>
+      <div className={`icon cashflows ${activeComponent === 'cashflows' ? 'active' : ''}`} onClick={() => handleComponentChange('cashflows')}>
         {activeComponent === 'cashflows' && <Cashflows />}
         <FontAwesomeIcon
           icon={faCommentsDollar}
           style={{ color: "#dc240d", width: "15%", height: "15%", padding: "20px" }}
-          onClick={() => handleComponentChange('cashflows')}
         />
         <label>Cashflow</label>
       </div>
